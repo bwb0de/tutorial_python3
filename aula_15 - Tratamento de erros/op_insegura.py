202950c4453fcbfe4d3b8a6dbea86e55044c62f6
Daniel Cruz <danielc@unb.br>
2025-06-02 17:56:25 -0300

    Salvando alterações no segundo exemplo de tratamento de erros...

--- aula_15 - Tratamento de erros/op_insegura.py
+++ aula_15 - Tratamento de erros/op_insegura.py
@@ -3,8 +3,8 @@
 print("Selecione uma opção de suco de frutas [-1 para sair]:")
 opcoes = ['maçã', 'laranja', 'uva']
 while True:
-	print(f"Digite um numero entre 0 e {len(opcoes)}...")
-	print(opcoes)
+	print(f"Digite um número inteiro para selecionar.")
+	print(opcoes, '\n')
 	opidx = int(input("$: "))
 	if opidx == -1: break
 	print(f"Você selecionou: {opcoes[opidx]}\n\n")
--- aula_15 - Tratamento de erros/op_segura.py
+++ aula_15 - Tratamento de erros/op_segura.py
@@ -1,12 +1,24 @@
-# Seletor de opções inseguro
+# Seletor de opções seguro
 
 print("Selecione uma opção de suco de frutas [-1 para sair]:")
 opcoes = ['maçã', 'laranja', 'uva']
 while True:
-	print(f"Digite um numero entre 0 e {len(opcoes)}...")
-	print(opcoes)
-	opidx = int(input("$: "))
-	if opidx == -1: break
-	print(f"Você selecionou: {opcoes[opidx]}\n\n")
-	break
+	try:
+		print(f"Digite um número inteiro para selecionar.")
+		print(opcoes, '\n')
+		opidx = int(input("$: "))
+		if opidx == -1: break
+		print(f"Você selecionou: {opcoes[opidx]}\n\n")
+		break
+	
+	
+	except IndexError:
+		print(f"\nVocê digitou um índice inválido, o valor deve estar entre 0 e {len(opcoes) - 1}...\n")
+		continue
+	except ValueError:
+		print(f"\nO valor fornecido precisa ser numérico, digite -1 se quiser encerar o programa...\n")
+		continue
+	except KeyboardInterrupt:
+		print(f"\nPrograma finalizado [Ctrl+C]...")
+		break
 
