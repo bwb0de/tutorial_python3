43065bb0fe35aa91abffd405e5b2f6e11d2aaf04
Daniel Cruz <danielc@unb.br>
2025-06-03 16:21:06 -0300

    Acrescentendo outra atividade...

---
+++ aula_06 - Tipos compostos com hash - set, dict/02_massa_gases.py
@@ -0,0 +1,18 @@
+#
+# Crie um programa que receba 2 inputs, cada um constituído de um número e um símbolo
+# representando um dos gases nobres da tabela periódica. O número e o símbolo de cada
+# input deve ser separado por um espaço. O programa deverá retornar o valor da massa
+# atômica total conforme a quantidade e os tipos de gás indicado.
+#
+# Dica: usar dicionário para registrar, no código, os valores de massa dos gases nobres.
+#
+# Massas aproximadas: 
+#     He (Hélio)       =>   4.003
+#     Ne (Neônio)      =>  20.180
+#     Ar (Argônio)     =>  39.948
+#     Kr (Criptônio)   =>  83.798
+#     Xe (Xenônio)     => 131.293
+#     Rn (Radônio)     => 220.000
+#     Og (Oganessônio) => 294.000
+#
+
