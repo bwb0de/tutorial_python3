508877d0548fd534784f0ea8a1498440e2629f71
Daniel Cruz <danielc@unb.br>
2025-06-11 22:36:53 -0300

    Exercícios aula 12

--- aula_12 - Modularização e testes/estatistica.py
+++ aula_12 - Modularização e testes/estatistica.py
@@ -1,23 +1,17 @@
-def media_aritmetica():
-	pass
+def calcular_media_aritmetica(lista_numeros, precisao):
+    pass
 
+def identificar_moda(lista_numeros):
+    pass
+    
+def calcular_mediana(lista_numeros):
+    pass
 
-def moda():
-	pass
+def calcular_desvios(lista_numeros, precisao):
+    pass
 
-	
-def mediana():
-	pass
+def calcular_variancia(lista_numeros, precisao):
+    pass
 
-	
-def desvios():
-	pass
-
-	
-def variancia():
-	pass
-
-	
-def desvio_padrao():
-	pass
-
+def calcular_desvio_padrao(lista_numeros, precisao):
+    pass
--- aula_12 - Modularização e testes/teste_estatistica.py
+++ aula_12 - Modularização e testes/teste_estatistica.py
@@ -1,77 +1,79 @@
-from estatistica import moda, mediana, media_aritmetica, desvios, variancia, desvio_padrao
+from estatistica import identificar_moda, calcular_mediana, calcular_media_aritmetica, calcular_desvios, calcular_variancia, calcular_desvio_padrao
 
 
 def test_moda():
-	print("Testando função moda() ", end="")
-	try:
-		assert(moda([2,3,7,4,9,8,1,4,6,5,3,4]) == 4)
-		print("=> Ok!")
-	except TypeError:
-		print("=> Falhou! A função foi implementada?")
-	
+    print("Testando função identificar_moda() ", end="")
+    try:
+        assert(identificar_moda([2,3,7,4,9,8,1,4,6,5,3,4]) == [4])
+        assert(identificar_moda([2,3,7,4,3,8,1,4,6,5,3,4]) == [3,4])
+        assert(identificar_moda([3,3,7,4,9,8,9,4,9,5,3,4]) == [3,4,9])
+        print("=> Ok!")
+    except TypeError:
+        print("=> Falhou! A função foi implementada?")
+    
 
 def test_media_aritmetica():
-	print("Testando função media_aritmetica() ", end="")
-	try:
-		assert(media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], 2) == 4.67)
-		assert(media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], 3) == 4.667)
-		assert(media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], 4) == 4.6667)
-		assert(media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], False) == 4.666666666666667)
-		print("=> Ok!")
-	except TypeError:
-		print("=> Falhou! A função foi implementada?")
+    print("Testando função calcular_media_aritmetica() ", end="")
+    try:
+        assert(calcular_media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], 2) == 4.67)
+        assert(calcular_media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], 3) == 4.667)
+        assert(calcular_media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], 4) == 4.6667)
+        assert(calcular_media_aritmetica([2,3,7,4,9,8,1,4,6,5,3,4], False) == 4.666666666666667)
+        print("=> Ok!")
+    except TypeError:
+        print("=> Falhou! A função foi implementada?")
 
-	
+    
 def test_mediana():
-	print("Testando função mediana() ", end="")
-	try:
-		assert(mediana([2,3,7,4,9,8,1,4,6,5,3,4]) == 4)
-		print("=> Ok!")
-	except TypeError:
-		print("=> Falhou! A função foi implementada?")
-	
-	
+    print("Testando função calcular_mediana() ", end="")
+    try:
+        assert(calcular_mediana([2,3,7,4,9,8,1,4,6,5,3,4]) == 4)
+        print("=> Ok!")
+    except TypeError:
+        print("=> Falhou! A função foi implementada?")
+    
+    
 def test_desvios():
-	print("Testando função desvios() ", end="")
-	try:
-		assert(desvios([2,3,7,4,9,8,1,4,6,5,3,4], 2) == [-3.67, -2.67, -1.67, -1.67, -0.67, -0.67, -0.67, 0.33, 1.33, 2.33, 3.33, 4.33])
-		assert(desvios([2,3,7,4,9,8,1,4,6,5,3,4], 3) == [-3.667, -2.667, -1.667, -1.667, -0.667, -0.667, -0.667,  0.333, 1.333,  2.333,  3.333,  4.333])
-		assert(desvios([2,3,7,4,9,8,1,4,6,5,3,4], 4) == [-3.6667, -2.6667, -1.6667, -1.6667, -0.6667, -0.6667, -0.6667, 0.3333,  1.3333,  2.3333,  3.3333,  4.3333])
-		assert(desvios([2,3,7,4,9,8,1,4,6,5,3,4], False) == [-3.666666666666667, -2.666666666666667, -1.666666666666667, -1.666666666666667, -0.666666666666667, -0.666666666666667, -0.666666666666667, 0.33333333333333304, 1.333333333333333, 2.333333333333333, 3.333333333333333, 4.333333333333333])
-		print("=> Ok!")
-	except TypeError:
-		print("=> Falhou! A função foi implementada?")
+    print("Testando função calcular_desvios() ", end="")
+    try:
+        assert(calcular_desvios([2,3,7,4,9,8,1,4,6,5,3,4], 2) == [-3.67, -2.67, -1.67, -1.67, -0.67, -0.67, -0.67, 0.33, 1.33, 2.33, 3.33, 4.33])
+        assert(calcular_desvios([2,3,7,4,9,8,1,4,6,5,3,4], 3) == [-3.667, -2.667, -1.667, -1.667, -0.667, -0.667, -0.667,  0.333, 1.333,  2.333,  3.333,  4.333])
+        assert(calcular_desvios([2,3,7,4,9,8,1,4,6,5,3,4], 4) == [-3.6667, -2.6667, -1.6667, -1.6667, -0.6667, -0.6667, -0.6667, 0.3333,  1.3333,  2.3333,  3.3333,  4.3333])
+        assert(calcular_desvios([2,3,7,4,9,8,1,4,6,5,3,4], False) == [-3.666666666666667, -2.666666666666667, -1.666666666666667, -1.666666666666667, -0.666666666666667, -0.666666666666667, -0.666666666666667, 0.33333333333333304, 1.333333333333333, 2.333333333333333, 3.333333333333333, 4.333333333333333])
+        print("=> Ok!")
+    except TypeError:
+        print("=> Falhou! A função foi implementada?")
 
 
 def test_variancia():
-	print("Testando função variancia() ", end="")
-	try:
-		assert(variancia([2,3,7,4,9,8,1,4,6,5,3,4], 2) == 5.88)	
-		assert(variancia([2,3,7,4,9,8,1,4,6,5,3,4], 3) == 5.879)	
-		assert(variancia([2,3,7,4,9,8,1,4,6,5,3,4], 4) == 5.8788)	
-		assert(variancia([2,3,7,4,9,8,1,4,6,5,3,4], False) == 5.878787878787879)	
-		print("=> Ok!")
-	except TypeError:
-		print("=> Falhou! A função foi implementada?")
+    print("Testando função calcular_variancia() ", end="")
+    try:
+        assert(calcular_variancia([2,3,7,4,9,8,1,4,6,5,3,4], 2) == 5.88)    
+        assert(calcular_variancia([2,3,7,4,9,8,1,4,6,5,3,4], 3) == 5.879)    
+        assert(calcular_variancia([2,3,7,4,9,8,1,4,6,5,3,4], 4) == 5.8788)    
+        assert(calcular_variancia([2,3,7,4,9,8,1,4,6,5,3,4], False) == 5.878787878787879)    
+        print("=> Ok!")
+    except TypeError:
+        print("=> Falhou! A função foi implementada?")
 
 
 def test_desvio_padrao():
-	print("Testando função desvio_padrao() ", end="")
-	try:
-		assert(desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], 2) == 2.42)
-		assert(desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], 3) == 2.425)
-		assert(desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], 4) == 2.4246)
-		assert(desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], False) == 2.424621182533032)
-		print("=> Ok!")
-	except TypeError:
-		print("=> Falhou! A função foi implementada?")
+    print("Testando função calcular_desvio_padrao() ", end="")
+    try:
+        assert(calcular_desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], 2) == 2.42)
+        assert(calcular_desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], 3) == 2.425)
+        assert(calcular_desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], 4) == 2.4246)
+        assert(calcular_desvio_padrao([2,3,7,4,9,8,1,4,6,5,3,4], False) == 2.424621182533032)
+        print("=> Ok!")
+    except TypeError:
+        print("=> Falhou! A função foi implementada?")
 
 
 
 if __name__ == '__main__':
-	test_moda()
-	test_mediana()
-	test_media_aritmetica()
-	test_desvios()
-	test_variancia()
-	test_desvio_padrao()
+    test_moda()
+    test_mediana()
+    test_media_aritmetica()
+    test_desvios()
+    test_variancia()
+    test_desvio_padrao()
