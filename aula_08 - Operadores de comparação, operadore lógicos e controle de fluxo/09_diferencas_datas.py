06f306eadf609d8526189ade772618a0d27b3a03
Daniel Cruz <danielc@unb.br>
2025-12-30 11:11:10 -0300

    Exercício datas

---
+++ aula_08 - Operadores de comparação, operadore lógicos e controle de fluxo/09_diferencas_datas.py
@@ -0,0 +1,12 @@
+#
+# Crie um programa que retorne a diferença em dias entre duas datas.
+# As datas fornecidas deverão ser do mesmo ano.
+# As datas fornecidas deverão ser válidas.
+# Considere os anos bissextos.
+#   - Um ano é bissexto se for divisível por 4 e não for divisível por 100
+#   - Ou se for dicisível por 400
+
+# Não utilize os módulos nativos do python!
+#
+
+
