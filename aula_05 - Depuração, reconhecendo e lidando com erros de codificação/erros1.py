8454767d622ca00436080e8ad893275d4295b6a5
Daniel Cruz <danielc@unb.br>
2025-12-31 14:11:41 -0300

    Corrigindo ortografia

---
+++ aula_05 - Depuração, reconhecendo e lidando com erros de codificação/erros1.py
@@ -0,0 +1,11 @@
+valor_fornecido = input("Digite seu nome:")
+valor_fornecido.upper()
+ano_nascimento = input("Ano nascimento:")
+ano_corrente = 2025
+idade = ano_corrente - int(ano_nascimento)
+arquivo_registro = open("nomes_idades.txt", 'a')
+arquivo_registro.write(valor_fornecido+',')
+arquivo_registro.write(';')
+arquivo_registro.close()
+print(f"Oi {valor_fornecido}, sua idade é aproximadamente {idade} anos.")
+
