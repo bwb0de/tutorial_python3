2f3a9544a7f7d6ee88c32ccb5624af40bf390176
Daniel Cruz <danielc@unb.br>
2026-01-15 16:11:57 -0300

    Novo exercício usando subprocess

--- aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns métodos de strings/09_sub_shell_filter.py
+++ aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns métodos de strings/09_sub_shell_filter.py
@@ -1,7 +1,7 @@
 #
 # Crie um programa que leia uma lista de frutas 'frutas.txt' e apresente
 # apenas as frutas que iniciam com a letra 'A'. Os nomes das frutas listadas 
-# ser apresentados em letra maiúscula.
+# devem ser apresentados em letra maiúscula.
 # 
 #
 # Dica: use o módulo 'subprocess' e métodos de string
