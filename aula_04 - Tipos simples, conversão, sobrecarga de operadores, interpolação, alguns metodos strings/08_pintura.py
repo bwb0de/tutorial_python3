77234f0043f6fd1995f0400937631939612ca8b9
Daniel Cruz <danielc@unb.br>
2026-02-04 08:22:34 -0300

    Ajustando resposta conforme novo parâmetro de área pintada

--- aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns metodos strings/08_pintura.py
+++ aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns metodos strings/08_pintura.py
@@ -5,23 +5,23 @@
 # Informações relevantes:
 #  - altura do pé direito
 #  - soma da metragem linear das paredes
-#  - uma lata de tinta pinta 2.25 m² de parede
+#  - uma lata de tinta pinta 22.5 m² de parede
 #  - desconte a área de portas e janelas 
 #  - assuma que todas portas e janelas tem o mesmo tamanho
 #
 
-AREA_PINTURA_POR_LATA = 2.25
+AREA_PINTURA_POR_LATA = 22.5
 
-pe_direito = float(input("Altura do pé direito em metros: ").replace(",","."))
-comprimento_linear_paredes = float(input("Soma do comprimento linear das paredes em metros: ").replace(",","."))
+pe_direito = float(input("Altura do pé direito em metros: ").strip().replace(",","."))
+comprimento_linear_paredes = float(input("Soma do comprimento linear das paredes em metros: ").strip().replace(",","."))
 
-janela_largura = float(input("Largura janela em metros: ").replace(",","."))
-janela_altura = float(input("Altura janela em metros: ").replace(",","."))
-janela_qtd = int(input("Número de janelas na área a ser pintada: "))
+janela_largura = float(input("Largura janela em metros: ").strip().replace(",","."))
+janela_altura = float(input("Altura janela em metros: ").strip().replace(",","."))
+janela_qtd = int(input("Número de janelas na área a ser pintada: ").strip())
 
-porta_largura = float(input("Largura porta em metros: ").replace(",","."))
-porta_altura = float(input("Altura porta em metros: ").replace(",","."))
-porta_qtd = int(input("Número de portas na área a ser pintada: "))
+porta_largura = float(input("Largura porta em metros: ").strip().replace(",","."))
+porta_altura = float(input("Altura porta em metros: ").strip().replace(",","."))
+porta_qtd = int(input("Número de portas na área a ser pintada: ").strip())
 
 area_inicial = pe_direito * comprimento_linear_paredes
 area_descontada_janelas = janela_largura * janela_altura * janela_qtd
