9bb35d9336b3644fa9c37d36c4ffa3a552e03dfc
Daniel Cruz <danielc@unb.br>
2026-02-04 08:11:19 -0300

    Ajuste na informação de área de pintura

--- aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns métodos de strings/08_pintura.py
+++ aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns métodos de strings/08_pintura.py
@@ -5,7 +5,7 @@
 # Informações relevantes:
 #  - altura do pé direito
 #  - soma da metragem linear das paredes
-#  - uma lata de tinta pinta 2.25 m² de parede
+#  - uma lata de tinta pinta 22.5 m² de parede (3 aplicações)
 #  - desconte a área de portas e janelas 
 #  - assuma que todas portas e janelas tem o mesmo tamanho
 #
