9955c068155d8d9b3fd023b080fc1e4147629949
Daniel Cruz <danielc@unb.br>
2026-02-04 08:14:01 -0300

    Exemplos taxa de conversão
    Added conversion rates for USD, Argentine peso, and Euro.

--- aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns metodos strings/07_conversoes.py
+++ aula_04 - Tipos simples, conversão, sobrecarga de operadores, interpolação, alguns metodos strings/07_conversoes.py
@@ -2,6 +2,11 @@
 # Crie um programa que receba um valor em reais e apresente na tela o valor
 # convertido em três moedas diferentes
 #
+# Exemplos taxa conversão:
+# dolar turismo 5.863
+# peso argentino 0.005
+# euro 6.430
+#
 
 valor_reais = float(input("Digiter um valor em R$: ").replace(",","."))
 
